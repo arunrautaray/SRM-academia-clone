a64f67a6e086b08cf15dc52d67bde8aabe43fbb5
Arun Rautaray <ar6071@srmist.edu.in>
2022-02-28 23:59:55 +0530

    Academia Clone Built Using Angular.JS

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,22 @@
-import { NgModule } from '@angular/core';
+import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {RouterModule} from '@angular/router';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { AppComponent }   from './app.component';
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { DefaultModule } from './layouts/default/default.module';
+
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
+  imports:[
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    DefaultModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [ AppComponent],
+  bootstrap:    [ AppComponent ]
 })
+
 export class AppModule { }
